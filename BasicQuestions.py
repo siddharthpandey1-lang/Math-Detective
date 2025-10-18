965006fd86677fe9ed1b5cb3db552d25765142ae
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-18 22:09:53 +0530

    Questions

---
+++ BasicQuestions.py
@@ -0,0 +1,19 @@
+print("Here is your first puzzle:")
+print("Indentify the wrong number!")
+print("2, 3, 5, 7, 9, 11")
+answer = input("Your answer: ")
+if answer == "9":
+    print("Correct! 9 is not a prime number.")
+else:
+    print("Incorrect. The correct answer is 9, as it is not a prime number.")
+print("Here is your second puzzle:")
+print("What is the next number in the sequence?")
+print("1, 1, 2, 3, 5, 8, ?")
+answer = input("Your answer: ")
+if answer == "13":
+    print("Correct! The next number is 13, following the Fibonacci sequence.")
+else:
+    print("Incorrect. The correct answer is 13, following the Fibonacci sequence")
+print("What is fibonacci sequence?")
+print("A number sequence where each number is the sum of the two preceding ones, usually starting with 0 and 1.")
+print("Great job on completing the basic questions!")
