5e609597c89fafbadec49bf911e551a52374a8f4
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-19 12:02:08 +0530

    FinalLevel.py

---
+++ level4.py
@@ -0,0 +1,21 @@
+print("Now this is the final level!")
+print("Now the questions would be challenging.")
+print("So lets start!")
+print("What is fibbonaci of 7?")
+answer = input("Your answer: ")
+if answer == "13":
+    print("Correct!")
+else:
+    print("Wrong! The correct answer is 13")
+print("What is 12*12?")
+answer = input("Your answer: ")
+if answer == "144":
+    print("Correct!")
+else:
+    print("Wrong! The correct answer is 144 because * is multiplication sign")
+    print("You have completed Level 4!")
+print("Congratulations on completing the game!")
+print("You are a Math Detective now!")
+print("Thank you for playing Math Detective!")
+print("Goodbye!")
+print("End of Level 4")
